43c68a9296689f9c51e1e7c1ca56d6dcdfc50342
Niccolo Rigacci <niccolo@rigacci.org>
2017-02-16 12:25:06 +0100

    Added code to read data from PostgreSQL too

--- html/getpmdata.php
+++ html/getpmdata.php
@@ -1,8 +1,12 @@
 <?php
 
-// Extract one year data back from today.
+require_once('config.php');
+
+// Extract one year of data, back from today.
+// Strings represent dates in localtime (PHP-server idea of).
 $from_day = date('Y-m-d', strtotime('1 year ago', time()));
 $to_day = date('Y-m-d', time());
+$tomorrow = date('Y-m-d', strtotime('1 day', time()));
 
 // Use a color classification like ARPAT Toscana.
 function color_arpat_like($val) {
@@ -16,19 +20,56 @@ function color_arpat_like($val) {
 
 $data = array();
 
-$db = new SQLite3('/var/lib/airpi/airpi-data.db');
-$sql  = "SELECT strftime('%%Y-%%m-%%d', timestamp, 'localtime') AS day, avg(value) AS pm10";
-$sql .= " FROM data WHERE day >= '%s' AND day <= '%s' AND type = 'pm10' GROUP BY day ORDER BY day";
-$sql = sprintf($sql, $from_day, $to_day);
-//print "$sql\n";
-$result = $db->query($sql);
-// For JavaScript Date(): january is month 0.
-while($res = $result->fetchArray(SQLITE3_ASSOC)) {
-    $year  = (int)substr($res['day'], 0, 4);
-    $month = (int)substr($res['day'], 5, 2) - 1;
-    $day   = (int)substr($res['day'], 8, 2);
-    $val   = (float)$res['pm10'];
-    $data[] = array($year, $month, $day, $val, color_arpat_like($val));
+if (PG_CONNECT != '') {
+    // Use PostgreSQL database.
+    // NOTICE: time_stamp field in database is without timezone, it is converted to timezone-aware
+    // using the "AT TIME ZONE" construct and presented in localtime (PosgresSQL idea of). To
+    // change PostgreSQL idea of localtime use something like: SET TIME ZONE 'America/Los_Angeles';
+    // Query parameters are casted to timestamp without timezone and comparison is performed
+    // against presented localtime, disregarding timezone.
+    if (($db = pg_connect(PG_CONNECT)) !== FALSE) {
+        $sql  = "SELECT to_char(time_stamp AT TIME ZONE 'UTC', 'YYYY-MM-DD') AS day, avg(value) AS pm10 FROM data";
+        $sql .= " WHERE (time_stamp AT TIME ZONE 'UTC') >= '%s'::TIMESTAMP";
+        $sql .= " AND (time_stamp AT TIME ZONE 'UTC') < '%s'::TIMESTAMP";
+        $sql .= " AND type = 'pm10' GROUP BY day ORDER BY day";
+        $sql = sprintf($sql, $from_day, $tomorrow);
+        //print "$sql\n";
+        $result = pg_query($db, $sql);
+        while($row = pg_fetch_assoc($result)) {
+            //print "Data: " . gettype($row['day']) . ", " . $row['day'] . "\n";
+            $year  = (int)substr($row['day'], 0, 4);
+            $month = (int)substr($row['day'], 5, 2) - 1;
+            $day   = (int)substr($row['day'], 8, 2);
+            $val   = (float)$row['pm10'];
+            $data[] = array($year, $month, $day, $val, color_arpat_like($val));
+        }
+    }
+} else {
+    // Use SQLite database.
+    // NOTICE: timestamp field in database is TEXT in ISO 8601 UTC time format ('%Y-%m-%dT%H:%M:%SZ'),
+    // it is converted to localtime using the 'localtime' modifier, and then converted to a
+    // formatted string with strftime() function. SQLite idea of localtime comes from the operating
+    // system, e.g. on Debian GNU/Linux change it with "dpkg-reconfigure tzdata".
+    try {
+        $db = new SQLite3('/var/lib/airpi/airpi-data.db');
+    } catch (Exception $e) {
+        error_log('new SQLite3() Exception: ' . $e->getMessage());
+    }
+    if ($db) {
+        $sql  = "SELECT strftime('%%Y-%%m-%%d', timestamp, 'localtime') AS day, avg(value) AS pm10";
+        $sql .= " FROM data WHERE day >= '%s' AND day <= '%s' AND type = 'pm10' GROUP BY day ORDER BY day";
+        $sql = sprintf($sql, $from_day, $to_day);
+        //print "$sql\n";
+        $result = $db->query($sql);
+        // For JavaScript Date(): january is month 0.
+        while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $year  = (int)substr($row['day'], 0, 4);
+            $month = (int)substr($row['day'], 5, 2) - 1;
+            $day   = (int)substr($row['day'], 8, 2);
+            $val   = (float)$row['pm10'];
+            $data[] = array($year, $month, $day, $val, color_arpat_like($val));
+        }
+    }
 }
 
 header('Content-type: application/json');
