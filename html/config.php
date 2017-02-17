f562b40c3c1e0b1e5ba4489a47a612d6ba70be27
Niccolo Rigacci <niccolo@rigacci.org>
2017-02-17 06:34:04 +0100

    Added some config defaults

--- html/config.php
+++ html/config.php
@@ -2,5 +2,7 @@
 
 define('STATION_NAME', 'Via Alessandro Volta, Prato');
 define('APP_TITLE',    'Centralina AirPi');
+define('PG_CONNECT', '');   // Leave blank to use SQLite.
+define('STALE_RRD', 900);   // Ignore RRD data if older than (seconds).
 
 ?>
