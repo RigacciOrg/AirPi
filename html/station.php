a75224c1c7b22e7ed995414eb20671dabe323706
Niccolo Rigacci <niccolo@rigacci.org>
2017-02-25 17:21:27 +0100

    Added favicons, etc

--- html/station.php
+++ html/station.php
@@ -23,6 +23,12 @@ switch ($page) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="Niccolo Rigacci <niccolo@rigacci.org>">
+    <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="icons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="icons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
 <?php
 if ($refresh) echo '    <meta http-equiv="refresh" content="' . $refresh . '">' . "\n";
 ?>
