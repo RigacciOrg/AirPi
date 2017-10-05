84610b41a8a834811f937cfc6cf21a05ccd2faab
Niccolo Rigacci <niccolo@rigacci.org>
2017-10-05 07:05:50 +0200

    Fixed typo in config file, changed default station name

--- html/config-sample.php
+++ html/config-sample.php
@@ -4,7 +4,7 @@
 // It is used by an AirPi web datacenter, to show different stations.
 $station_id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '0';
 
-define('STATION_NAME', 'Via Alessandro Volta, Prato');
+define('STATION_NAME', 'My AirPi Station');
 define('APP_TITLE',    'AirPi Station');
 define('PG_CONNECT', '');   // Leave blank to use SQLite.
 define('STALE_RRD', 900);   // Ignore RRD data if older than (seconds).
